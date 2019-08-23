ebf35f60acb2cec1f59dc0081e7b23fb583c717b
Kevin-it237 <ngaleuabel@gmail.com>
2019-08-23 13:33:14 +0100

    Front

--- src/App.js
+++ src/App.js
@@ -13,30 +13,23 @@ import SupplierRevervation from './ui/components/Supplier/SupplierRegistration';
 import Admin from './ui/admin/Admin';
 import AdminLogin from './ui/admin/AdminLogin/AdminLogin';
 
-class App extends React.PureComponent {
+function App() {
+  return (
+    <BrowserRouter>
+        <Route path="/" exact component={Home}  />
+        <Route path="/auth" exact component={AuthPage}  />
+        <Route path="/annonces" exact component={AllEventsPage}  />
+        <Route path="/services" exact component={AllServicesPage}  />
+        <Route path="/supplier" exact component={SupplierRevervation}  />
+        <Route path="/category/:id" component={SingleCategoryPage}  />
+        <Route path="/service/details/:id" component={DetailsPage}  />
+        <Route path="/dashboard/reservations" component={SupplierDashboard}  />
 
-  componentWillMount(){
-    document.title = "Entre Coops"
-  }
-
-  render() {
-    return (
-      <BrowserRouter>
-          <Route path="/" exact component={Home}  />
-          <Route path="/auth" exact component={AuthPage}  />
-          <Route path="/annonces" exact component={AllEventsPage}  />
-          <Route path="/services" exact component={AllServicesPage}  />
-          <Route path="/supplier" exact component={SupplierRevervation}  />
-          <Route path="/category/:id" component={SingleCategoryPage}  />
-          <Route path="/service/details/:id" component={DetailsPage}  />
-          <Route path="/dashboard/reservations" component={SupplierDashboard}  />
-  
-          {/* Admin Routes */}
-          <Route path="/auth/admin" exact component={AdminLogin} />
-          <Route path="/admin" component={Admin} />
-      </BrowserRouter>
-    );
-  }
+        {/* Admin Routes */}
+        <Route path="/auth/admin" exact component={AdminLogin} />
+        <Route path="/admin" component={Admin} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
