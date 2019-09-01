2019ad15d5538802f90b9bcb1d753d4e67b4e38e
kevin-it <ngaleuabel@gmail.com>
2019-09-01 23:48:54 +0200

    add server

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+// Connect to db
+mongoose.connect(config.database, { useNewUrlParser: true });
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function() {
+    console.log('Connected to mongodb');
+})
+
+// App initialization
+const app = express();
+
+app.get('/', function(req, res) {
+    res.send("Working")
+})
+
+// Start the app
+const PORT = 3000;
+app.listen(PORT, function() {
+    console.log("Server started on port " + PORT)
+})
