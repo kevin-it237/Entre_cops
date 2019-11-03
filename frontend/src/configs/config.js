7ed6a0c17ba3df978d2f8679608a872c5298bed1
kevin-it <ngaleuabel@gmail.com>
2019-11-03 13:09:31 +0100

    First build version

--- frontend/src/configs/config.js
+++ frontend/src/configs/config.js
@@ -1,2 +1,2 @@
-export const rootUrl = "http://localhost:3000";
-// export  const rootUrl = "https://entrecops.co";
+//export const rootUrl = "http://localhost:3000";
+export  const rootUrl = "https://entrecops.co";
